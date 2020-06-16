86dd57625fa8a6e01f42c6964b8af2eb44bc2e0c
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-16 11:24:04 -0300

    connecting to database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+    useNewUrlParser: true,  
+    useUnifiedTopology: true  
+});
 
 app.get('/', (request, response) => {
     response.send('Hello world');
