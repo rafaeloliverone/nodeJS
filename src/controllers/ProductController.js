5a6806764d4d34f4b7e8e37819a420000642ae9a
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-17 00:27:34 -0300

    added store method

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
-    
+
     async index(request, response) {
         const products = await Product.find();
 
         return response.json(products);
+    },
+
+    async store(request, response) {
+        const product = await Product.create(request.body); 
+
+        return response.json(product);
     }
 
-
-}
+};
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
-routes.get('/', ProductController.index );
+routes.get('/', ProductController.index);
+routes.post('/', ProductController.store);
 
 module.exports = routes;
